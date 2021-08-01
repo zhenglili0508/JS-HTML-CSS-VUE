5e0c924541e5a202cf9f5cafa666c88b337bd870
zll <1278186879@qq.com>
2021-08-01 20:27:31 +0800

    添加

---
+++ 力扣刷题/面试/爱奇艺1.js
@@ -0,0 +1,56 @@
+function functionZZ(num) {
+
+    var map  = new  Map()
+    var set_index = new  Set()
+    for(let i = 0 ;i<6;i++){
+        if(map.has(num[i])){
+            set_index.delete(map.get(nums[i]))
+        }else{
+            map.set(num[i],i)
+            set_index.add(i)
+        }
+    }
+    return set_index
+}
+
+let nums = [1,2,3,4,1,2]
+// console.log(functionZZ(nums))
+
+let arr = '1 2 3 4 1 2'
+let arre = arr.split(' ')
+// console.log(arre)
+
+//set 的迭代与顺序
+let set = new Set(['value1','value2','value3'])
+let map = new Map([['key1','mapVal1'],['key2','mapVal2'],['key3','mapVal3']])
+// map 与set 的循环遍历 
+for(let value of set.values()){
+    console.log(value)
+}
+for(let mapKeyValue of map){
+ console.log(mapKeyValue)
+}
+for(let mapKey of map.keys()){
+    console.log(mapKey)
+}
+for(let mapValue of map.values()){
+    console.log(mapValue)
+}
+// map 与set 都有forEach
+set.forEach((value,dupval)=>{
+console.log(value,dupval)
+})
+map.forEach((key,value)=>{
+    console.log(key,'--->',value)
+})
+
+// map 与set 都有.size属性！！
+console.log('---------------size')
+console.log(set.size)
+console.log(map.size)
+
+//转为数组
+console.log([...set])
+
+
+
---
+++ 力扣刷题/面试/爱奇艺2.js
@@ -0,0 +1,14 @@
+var removeDuplicates = function(s) {
+    let stack = []
+   for(let i = 0; i < s.length; i++){
+       if(s[i] == stack[stack.length-1]){
+           stack.pop()
+       }else{
+           stack.push(s[i])
+       }
+   }
+   return stack.join('')
+ };
+
+ let  s = 'affggtteedfrrfdc'
+ console.log(removeDuplicates(s))
